b29b6709c6075a88fe81d4b33c9ecedcf0f553b4
Elric02 <elric.milet@gmail.com>
2025-03-26 03:05:11 +0100

    First video verification done

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ import read_protobuf
 import gtfs_realtime_pb2
 import math
 from pyproj import Geod
+from datetime import datetime
 
 
-# SINGLE START: only take data from 07:20:00
-def single_start():
+# SINGLE START: only take data from 07:16:00
+def single_start(filename):
     
     MessageType = gtfs_realtime_pb2.FeedMessage()
     single_start_df = read_protobuf.read_protobuf('../data/vehiclepositions/07/'+filename, MessageType)    # use file instead of bytes
@@ -51,68 +52,78 @@ def single_start():
 
 
 # ENTIRE HOUR: take all data from 07:20:00 to 08:19:59 included
-def entire_hour(single_start_df):
+def entire_hour():
 
-    def appendNewPBMinute(hour, minute, total_df, MessageType):
-        print("Now starting hour", hour, "minute", minute)
-        for second in range(0, 60):
-            second = str(second).zfill(2)
-            try:
-                filename = 'otraf-vehiclepositions-2022-03-22T'+hour+'-'+minute+'-'+second+'Z.pb'
-                temp_df = read_protobuf.read_protobuf('../data/vehiclepositions/'+hour+'/'+filename, MessageType)    # use file instead of bytes
-                temp_df = pd.DataFrame(temp_df['entity'].tolist())
-                temp_df['source'] = filename
+    def appendNewPBMinute(hour, minute, second, total_df, MessageType):
+        try:
+            filename = 'otraf-vehiclepositions-2022-03-22T'+hour+'-'+minute+'-'+second+'Z.pb'
+            temp_df = read_protobuf.read_protobuf('../data/vehiclepositions/'+hour+'/'+filename, MessageType)
+            temp_df = pd.DataFrame(temp_df['entity'].tolist())
+            temp_df['source'] = filename
 
-                # Exclude data outside the bus terminal
-                temp_df = temp_df[((temp_df['latitude'] > 58.416) & (temp_df['latitude'] < 58.419) & (temp_df['longitude'] > 15.621) & (temp_df['longitude'] < 15.626))]
-                routes_list = []
-                directions_list = []
-                route_short_name_list = []
-                route_type_list = []
-                for _, row in temp_df.iterrows():
-                    if not math.isnan(float(row['trip_id'])):
-                        trip = trips.loc[trips['trip_id'] == int(row['trip_id'])]
-                        if not trip.empty:
-                            routes_list.append(trip['route_id'].iloc[0])
-                            directions_list.append(trip['direction_id'].iloc[0])
-                            route_short_name_list.append(routes.loc[routes['route_id'] == float(trip['route_id'].iloc[0])]['route_short_name'].iloc[0])
-                            route_type_list.append(routes.loc[routes['route_id'] == float(trip['route_id'].iloc[0])]['route_type'].iloc[0])
-                        else:
-                            routes_list.append(np.nan)
-                            directions_list.append(np.nan)
-                            route_short_name_list.append(np.nan)
-                            route_type_list.append(np.nan)
+            # Exclude data outside the bus terminal
+            temp_df = temp_df[((temp_df['latitude'] > 58.416) & (temp_df['latitude'] < 58.419) & (temp_df['longitude'] > 15.621) & (temp_df['longitude'] < 15.626))]
+            routes_list = []
+            directions_list = []
+            route_short_name_list = []
+            route_type_list = []
+            for _, row in temp_df.iterrows():
+                if not math.isnan(float(row['trip_id'])):
+                    trip = trips.loc[trips['trip_id'] == int(row['trip_id'])]
+                    if not trip.empty:
+                        routes_list.append(trip['route_id'].iloc[0])
+                        directions_list.append(trip['direction_id'].iloc[0])
+                        route_short_name_list.append(routes.loc[routes['route_id'] == float(trip['route_id'].iloc[0])]['route_short_name'].iloc[0])
+                        route_type_list.append(routes.loc[routes['route_id'] == float(trip['route_id'].iloc[0])]['route_type'].iloc[0])
                     else:
-                        routes_list.append(np.nan)
-                        directions_list.append(np.nan)
-                        route_short_name_list.append(np.nan)
-                        route_type_list.append(np.nan)
-                temp_df['route_id'] = np.asarray(routes_list)
-                temp_df['direction_id'] = np.asarray(directions_list)
-                temp_df['route_short_name'] = np.asarray(route_short_name_list)
-                temp_df['route_type'] = np.asarray(route_type_list)
+                        routes_list.append(-1)
+                        directions_list.append(-1)
+                        route_short_name_list.append(-1)
+                        route_type_list.append(-1)
+                else:
+                    routes_list.append(-1)
+                    directions_list.append(-1)
+                    route_short_name_list.append(-1)
+                    route_type_list.append(-1)
+            temp_df['route_id'] = np.asarray(routes_list)
+            temp_df['direction_id'] = np.asarray(directions_list)
+            temp_df['route_short_name'] = np.asarray(route_short_name_list)
+            temp_df['route_type'] = np.asarray(route_type_list)
 
-                # Exclude non-bus data
-                temp_df = temp_df[(temp_df['route_type'] == 700)]
+            # Exclude non-bus data, but keep unknown-type data
+            temp_df = temp_df[((temp_df['route_type'] == 700) | (temp_df['route_id'] == -1))]
 
-                total_df = pd.concat([total_df, temp_df], ignore_index=True)
-            except FileNotFoundError:
-                print("File not found:", filename)
+            total_df = pd.concat([total_df, temp_df], ignore_index=True)
+        except FileNotFoundError:
+            print("File not found:", filename)
         return total_df
 
-    total_df = single_start_df
-    total_df['source'] = filename
+    total_df = pd.DataFrame()
     MessageType = gtfs_realtime_pb2.FeedMessage()
 
-    hour = "07"
-    for minute in range(20, 60):
-        minute = str(minute).zfill(2)
-        total_df = appendNewPBMinute(hour, minute, total_df, MessageType)
-        
-    hour = "08"
-    for minute in range(0, 20):
-        minute = str(minute).zfill(2)
-        total_df = appendNewPBMinute(hour, minute, total_df, MessageType)
+    # Input here desired time ranges to take into account. For example, [[7, 16, 0], [7, 32, 35]] is "from 07:16:00 to 07:32:35" (both included)
+    time_ranges = [
+        [[7, 16, 0], [7, 32, 35]],
+        [[7, 37, 25], [7, 53, 25]],
+        [[8, 11, 15], [8, 30, 00]],
+        [[8, 36, 35], [8, 38, 50]]
+    ]
+    
+    for time_range in time_ranges:
+        timestamp = 3600*time_range[0][0] + 60*time_range[0][1] + time_range[0][2]
+        max_timestamp = 3600*time_range[1][0] + 60*time_range[1][1] + time_range[1][2]
+        while timestamp <= max_timestamp:
+            hour = math.floor(timestamp/3600)
+            minute = math.floor((timestamp-hour*3600)/60)
+            second = timestamp-hour*3600-minute*60
+            if second == time_range[0][2]:
+                print("Now starting hour", hour, "minute", minute)
+            # Add an extra 0 where necessary (for example 08:22)
+            hour = str(hour).zfill(2)
+            minute = str(minute).zfill(2)
+            second = str(second).zfill(2)
+            total_df = appendNewPBMinute(hour, minute, second, total_df, MessageType)
+            timestamp += 1
 
     print(total_df)
     total_df.to_csv("entire_hour.csv")
@@ -143,23 +154,22 @@ def entire_hour_berths(entire_hour_df):
 # For now this is done manually 
 #static_data = pykoda.datautils.load_static_data('otraf', '2022_03_22', remove_unused_stations=True)
 
-filename = 'otraf-vehiclepositions-2022-03-22T07-20-00Z.pb'
 trips = pd.read_csv('../data/static/trips.txt')
 routes = pd.read_csv('../data/static/routes.txt')
 stops = pd.read_csv('../data/static/stops.txt')
 stop_times = pd.read_csv('../data/static/stop_times.txt')
 
-#single_start_df = single_start()
-#entire_hour_df = entire_hour(single_start_df.drop(single_start_df.index))
-#entire_hour_stopped_df = entire_hour_berths(entire_hour_df)
-single_start_df = pd.read_csv("single_start.csv")
-entire_hour_df = pd.read_csv("entire_hour.csv")
-entire_hour_stopped_df = pd.read_csv("entire_hour_berths.csv")
+#single_start_df = single_start('otraf-vehiclepositions-2022-03-22T07-16-00Z.pb')
+entire_hour_df = entire_hour()
+entire_hour_stopped_df = entire_hour_berths(entire_hour_df)
+#single_start_df = pd.read_csv("single_start.csv")
+#entire_hour_df = pd.read_csv("entire_hour.csv")
+#entire_hour_stopped_df = pd.read_csv("entire_hour_berths.csv")
 
 
 # Using static data to compare computed and detected berths
-
 results = []
+results_details = []
 vehicles = entire_hour_stopped_df['vehicle.id'].unique()
 for vehicle in vehicles:
     only_selected_vehicle = entire_hour_stopped_df.loc[entire_hour_stopped_df['vehicle.id'] == vehicle]
@@ -168,6 +178,7 @@ for vehicle in vehicles:
     counts = berth_shifts.cumsum().value_counts()
     # Get values of berths assigned at least 5x consecutively
     result = only_selected_vehicle[berth_shifts.cumsum().isin(counts[counts >= 5].index)]['assigned_berth'].unique()
+    result_with_times = only_selected_vehicle[berth_shifts.cumsum().isin(counts[counts >= 5].index)][['assigned_berth', 'timestamp']]
     # Get associated trips to this vehicle for this time period
     vehicle_trips = only_selected_vehicle['trip_id'].unique()
     
@@ -175,7 +186,10 @@ for vehicle in vehicles:
     directions = []
     computed_berths = []
     for vehicle_trip in vehicle_trips:
-        associated_trip_gtfs = trips.loc[trips['trip_id'] == vehicle_trip]
+        # Do not count not-in-service trips
+        if math.isnan(float(vehicle_trip)):
+            continue
+        associated_trip_gtfs = trips.loc[trips['trip_id'].apply(str) == str(vehicle_trip)]
         # Get associated routes short names
         route_id = associated_trip_gtfs['route_id'].iloc[0]
         route_short_name = routes.loc[routes['route_id'] == route_id]['route_short_name'].iloc[0]
@@ -184,7 +198,7 @@ for vehicle in vehicles:
         direction_id = associated_trip_gtfs['direction_id'].iloc[0]
         directions.append(direction_id)
         # Get associated berths at Linköpings Resecentrum
-        stops_in_trip = stop_times.loc[stop_times['trip_id'] == vehicle_trip]['stop_id']
+        stops_in_trip = stop_times.loc[stop_times['trip_id'].apply(str) == str(vehicle_trip)]['stop_id']
         lkpg_resecentrum = "90220050000500" # all stops at Linköping Centrum start with this sequence (and no other stop does)
         stops_in_trip_filtered = [stop for stop in stops_in_trip.tolist() if str(stop)[:len(lkpg_resecentrum)] == lkpg_resecentrum]
         # len(stops_in_trip_filtered) should always be 1, but maybe a trip could have two stops at the terminal
@@ -194,9 +208,12 @@ for vehicle in vehicles:
     computed_berths = pd.Series(computed_berths).unique().tolist()
     #print("trips id:", vehicle_trips, "routes id:", vehicle_routes, "directions:", directions, "computed berths:", computed_berths, "detected berths:", result)
     results.append({"vehicle": vehicle, "trips": vehicle_trips, "routes": vehicle_routes, "directions": directions, "computed": computed_berths, "detected": result})
+    results_details.append({"vehicle": vehicle, "trips": vehicle_trips, "routes": vehicle_routes, "directions": directions, "computed": computed_berths, "detected_details": result_with_times})
 
 results_df = pd.DataFrame(results)
+results_details_df = pd.DataFrame(results_details)
 print(results_df)
+results_df.to_csv('entire_hour_results.csv')
 exact_same = np.where((results_df['computed'].apply(set) & results_df['detected'].apply(set)), 1, 0)
 comparison = results_df.apply(lambda row: "same" if set(row['computed']) == set(row['detected']) else ("partial" if set(row['computed']) & set(row['detected']) else "different"), axis=1)
 print(comparison.value_counts())
@@ -210,3 +227,26 @@ test_df = pd.DataFrame(test_df['entity'].tolist())
 #print(test_df)
 #print(pd.DataFrame(test_df['stop_time_update'].tolist()))
 #pd.DataFrame(test_df['stop_time_update'].tolist()).to_csv("tripupdates_test.csv")
+
+
+# Prepare details list for video verification
+timemarks = []
+for i, vehicle in results_details_df.iterrows():
+    detected_details = vehicle['detected_details'].reset_index()
+    if detected_details.empty:
+        print("NO BERTH DETECTED FOR VEHICLE", vehicle['vehicle'], " ROUTES ", vehicle['routes'], " COMPUTED BERTHS ", vehicle['computed'], " FIRST SEEN ", datetime.fromtimestamp(int(str(entire_hour_stopped_df.loc[entire_hour_stopped_df['vehicle.id'] == vehicle['vehicle']]['timestamp'].iloc[0]))))
+        continue
+    timemarks.append({"vehicle": vehicle['vehicle'], "timestart": detected_details.iloc[0]['timestamp'], "detected_berth": detected_details.iloc[0]['assigned_berth'], "computed_berths_for_vehicle": vehicle['computed'], "route": vehicle['routes']})
+    current_time = detected_details.iloc[0]['timestamp']
+    for j, timeframe in detected_details.iterrows():
+        # If the timestamp of this row is >20 seconds later than the previous row, the vehicle probably moved in the meantime
+        if timeframe['timestamp'] > current_time + 20:
+            timemarks[-1]['timestop'] = detected_details.iloc[j-1]['timestamp']
+            timemarks.append({"vehicle": vehicle['vehicle'], "timestart": timeframe['timestamp'], "detected_berth": timeframe['assigned_berth'], "computed_berths_for_vehicle": vehicle['computed'], "route": vehicle['routes']})
+        current_time = timeframe['timestamp']
+    timemarks[-1]['timestop'] = detected_details.iloc[-1]['timestamp']
+timemarks_df = pd.DataFrame(timemarks).sort_values('timestart').reset_index()
+timemarks_df['timestart'] = timemarks_df['timestart'].apply(lambda x: datetime.fromtimestamp(int(str(x))))
+timemarks_df['timestop'] = timemarks_df['timestop'].apply(lambda x: datetime.fromtimestamp(int(str(x))))
+print(timemarks_df)
+timemarks_df.to_csv('for_video_verification.csv')
