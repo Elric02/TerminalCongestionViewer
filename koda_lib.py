e55eb5167b916d353022f4a10a735633b41e3c6e
Elric02 <elric.milet@gmail.com>
2025-09-18 17:06:40 +0200

    switch to polars in koda_lib.py

--- koda_lib.py
+++ koda_lib.py
@@ -1,7 +1,6 @@
 #TODO: import automatically data
-#TODO: switch to Polars
 
-import pandas as pd
+import polars as pl
 import numpy as np
 import read_protobuf
 import gtfs_realtime_pb2
@@ -29,25 +28,34 @@ def import_timeframe(provider, date, time_ranges, vehiclepositions_path, staticd
     :type export_type: str
     """
 
-    def appendNewPBMinute(hour, minute, second, total_df, MessageType, trips):
+    def appendNewPBSecond(hour, minute, second, total_df, MessageType, trips):
         try:
             filename = provider+'-vehiclepositions-'+date+'T'+hour+'-'+minute+'-'+second+'Z.pb'
             temp_df = read_protobuf.read_protobuf(vehiclepositions_path+'/'+date[0:4]+'/'+date[5:7]+'/'+date[8:]+'/'+hour+'/'+filename, MessageType)
-            temp_df = pd.DataFrame(temp_df['entity'].tolist())
-            temp_df['source'] = filename
+            temp_df = pl.DataFrame(temp_df['entity'].tolist())
+            temp_df = temp_df.with_columns(pl.lit(filename).alias("source"))
 
             routes_list = []
             directions_list = []
             route_short_name_list = []
             route_type_list = []
-            for _, row in temp_df.iterrows():
-                if not math.isnan(float(row['trip_id'])):
-                    trip = trips.loc[trips['trip_id'] == int(row['trip_id'])]
-                    if not trip.empty:
-                        routes_list.append(trip['route_id'].iloc[0])
-                        directions_list.append(trip['direction_id'].iloc[0])
-                        route_short_name_list.append(routes.loc[routes['route_id'] == str(trip['route_id'].iloc[0])]['route_short_name'].iloc[0])
-                        route_type_list.append(routes.loc[routes['route_id'] == str(trip['route_id'].iloc[0])]['route_type'].iloc[0])
+            for row in temp_df.iter_rows():
+                trip_id = row[temp_df.get_column_index('trip_id')]
+                if trip_id is not None:
+                    trip = trips.filter(pl.col("trip_id") == int(trip_id))
+                    if not trip.is_empty():
+                        route_id_val = trip.select("route_id").item()
+                        direction_id_val = trip.select("direction_id").item()
+
+                        routes_list.append(route_id_val)
+                        directions_list.append(direction_id_val)
+
+                        route = routes.filter(pl.col("route_id") == str(route_id_val))
+                        route_short_name_val = route.select("route_short_name").item()
+                        route_type_val = route.select("route_type").item()
+
+                        route_short_name_list.append(str(route_short_name_val))
+                        route_type_list.append(route_type_val)
                     else:
                         routes_list.append(-1)
                         directions_list.append(-1)
@@ -58,20 +66,22 @@ def import_timeframe(provider, date, time_ranges, vehiclepositions_path, staticd
                     directions_list.append(-1)
                     route_short_name_list.append(-1)
                     route_type_list.append(-1)
-            temp_df['route_id'] = np.asarray(routes_list)
-            temp_df['direction_id'] = np.asarray(directions_list)
-            temp_df['route_short_name'] = np.asarray(route_short_name_list)
-            temp_df['route_type'] = np.asarray(route_type_list)
-
-            total_df = pd.concat([total_df, temp_df], ignore_index=True)
+            temp_df = temp_df.with_columns([
+                pl.Series("route_id", routes_list),
+                pl.Series("direction_id", directions_list),
+                pl.Series("route_short_name", route_short_name_list, strict=False),
+                pl.Series("route_type", route_type_list),
+            ])
+            
+            total_df = pl.concat([total_df, temp_df], how="diagonal")
         except FileNotFoundError:
             print("File not found:", filename)
         return total_df
     
-    trips = pd.read_csv(staticdata_path+'/trips.txt')
-    routes = pd.read_csv(staticdata_path+'/routes.txt', dtype={'route_id': str})
+    trips = pl.read_csv(staticdata_path+'/trips.txt')
+    routes = pl.read_csv(staticdata_path+'/routes.txt', schema_overrides={'route_id': pl.Utf8})
 
-    total_df = pd.DataFrame()
+    total_df = pl.DataFrame()
     MessageType = gtfs_realtime_pb2.FeedMessage()
     
     for time_range in time_ranges:
@@ -88,9 +98,9 @@ def import_timeframe(provider, date, time_ranges, vehiclepositions_path, staticd
                 hour = str(hour).zfill(2)
                 minute = str(minute).zfill(2)
                 second = str(second).zfill(2)
-                total_df = appendNewPBMinute(hour, minute, second, total_df, MessageType, trips)
+                total_df = appendNewPBSecond(hour, minute, second, total_df, MessageType, trips)
             timestamp += 1
 
     if export_type == "csv":
-        total_df.to_csv("output/entire_hour_"+provider+"_"+date+"_test.csv")
+        total_df.write_csv("output/entire_hour_"+provider+"_"+date+"_test.csv")
     return total_df
