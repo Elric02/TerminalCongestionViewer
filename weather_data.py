c84b081f342504ff3b016ad42f3f0abe8ac5dcf5
Elric02 <elric.milet@gmail.com>
2026-01-09 15:20:27 +0100

    Weather data: improvements + first metrics

--- weather_data.py
+++ weather_data.py
@@ -1,26 +1,31 @@
 import requests
 import csv
 import polars as pl
+import matplotlib.pyplot as plt
 
 # Date(s) to fetch, output CSV path
 ENTIRE_YEAR = True # True to use TARGET_YEAR, False to use TARGET_DATE
 TARGET_DATE = "2025-05-02" # "YYYY-MM-DD"
 TARGET_YEAR = "2024" # "YYYY"
 DESIRED_STATIONS = [192840, 166910, 162860, 140460, 135460, 107420, 103100, 98230, 85240, 71420, 66110, 52240] # List of the IDs of the stations to include in the output. Leave empty if all stations desired
-METEO_PARAM = "9" # Number of the desired parameter to observe (e.g. "7" for amount ot precipitation aggregated per hour)
+METEO_PARAMS = ["1", "6", "7", "9"] # List of the desired parameters to observe (e.g. "7" is the amount ot precipitation aggregated per hour)
+DOWNLOAD_DATA = False # True if data to be requested to SMHI, False if CSVs are already there
 
-
-# Path for the output CSV
-OUTPUT_CSV = f"output/smhi_{METEO_PARAM}_{TARGET_YEAR}.csv" if ENTIRE_YEAR else f"output/smhi_precipitation_{TARGET_DATE}.csv"
+# Path for the output CSV containing raw data from SMHI
+OUTPUT_CSV_RAW = lambda param : f"output/smhi_{param}_{TARGET_YEAR}.csv" if ENTIRE_YEAR else f"output/smhi_{param}_{TARGET_DATE}.csv"
+# Path for the output CSV with analysis of all stations and parameters
+OUTPUT_CSV_ANALYSIS = f"output/smhi_analysis_{TARGET_YEAR}.csv" if ENTIRE_YEAR else f"output/smhi_analysis_{TARGET_DATE}.csv"
+# Parameters list URL
+PARAMETERS_URL = f"https://opendata-download-metobs.smhi.se/api/version/latest.json"
 # Stations list URL
-STATIONS_URL = f"https://opendata-download-metobs.smhi.se/api/version/latest/parameter/{METEO_PARAM}.json"
+STATIONS_URL = lambda param : f"https://opendata-download-metobs.smhi.se/api/version/latest/parameter/{param}.json"
 # Observations data URL
-OBS_URL = lambda id : f"https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/{METEO_PARAM}/station/{id}/period/corrected-archive/data.csv"
+OBS_URL = lambda param, id : f"https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/{param}/station/{id}/period/corrected-archive/data.csv"
 
 
 # Fetch all stations
-def get_all_stations():
-    resp = requests.get(STATIONS_URL)
+def get_all_stations(stations_url):
+    resp = requests.get(stations_url)
     resp.raise_for_status()
     stations = resp.json().get("station")
     # Build a dict mapping station id -> metadata
@@ -36,12 +41,12 @@ def get_all_stations():
     return station_meta
 
 # Fetch the data for each station
-def fetch_weather_for_date(station_meta):
+def fetch_weather_for_date(param, station_meta, obs_url_lambda):
     df = pl.DataFrame(schema={"Date": pl.Utf8, "Time": pl.Utf8, "Value": pl.Float32, "Quality": pl.Utf8, "StationID": pl.Utf8, "StationLatitude": pl.Float64, "StationLongitude": pl.Float64})
     i = 0
     for station_id in station_meta.keys():
         if len(DESIRED_STATIONS) > 0 and int(station_id) in DESIRED_STATIONS:
-            resp = requests.get(OBS_URL(station_id))
+            resp = requests.get(obs_url_lambda(param, station_id))
             resp.raise_for_status()
             data = resp.content.decode('utf-8')
             cr = csv.reader(data.splitlines(), delimiter=';')
@@ -71,12 +76,54 @@ def fetch_weather_for_date(station_meta):
             i += 1
     return df
 
+def plot_results(csv_path_lambda, parameters_url, meteo_params):
+    resp = requests.get(parameters_url)
+    resp.raise_for_status()
+    param_descs = dict([(x['key'], x['title']) for x in resp.json().get("resource")])
+
+    # Concatenate all CSVs into one DataFrame
+    dfs = []
+    for param in meteo_params:
+        df = (
+            pl.read_csv(csv_path_lambda(param))
+            .with_columns([
+                pl.col("Value").cast(pl.Float64),
+                pl.lit(param_descs[param]).alias("Source")
+            ])
+        )
+        dfs.append(df)
+    df_all = pl.concat(dfs)
+
+    # Compute interesting metrics per StationID
+    metrics = (
+        df_all
+        .group_by(["Source", "StationID"])
+        .agg([
+            pl.count().alias("count"),
+            pl.col("Value").min().alias("min"),
+            pl.col("Value").max().alias("max"),
+            pl.col("Value").mean().alias("mean"),
+            pl.col("Value").median().alias("median"),
+            pl.col("Value").std().alias("std"),
+            pl.col("Value").quantile(0.25).alias("q25"),
+            pl.col("Value").quantile(0.75).alias("q75"),
+        ])
+        .sort(["StationID", "Source"])
+    )
+    return metrics
+
+
 
 def main():
-    station_meta = get_all_stations()
-    weather_data = fetch_weather_for_date(station_meta)
-    weather_data.write_csv(OUTPUT_CSV)
-    print(f"Wrote weather data to {OUTPUT_CSV}")
+    if DOWNLOAD_DATA:
+        for param in METEO_PARAMS:
+            weather_data = fetch_weather_for_date(param, get_all_stations(STATIONS_URL(param)), OBS_URL)
+            weather_data.write_csv(OUTPUT_CSV_RAW(param))
+            print(f"Wrote weather data to {OUTPUT_CSV_RAW(param)}")
+
+    metrics = plot_results(OUTPUT_CSV_RAW, PARAMETERS_URL, METEO_PARAMS)
+    print(metrics)
+    metrics.write_csv(OUTPUT_CSV_ANALYSIS)
 
 if __name__ == "__main__":
     main()
