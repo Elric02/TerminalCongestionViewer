5b929c0f8ae9811026b4dc6b5e2d9b1adaa6d02f
Elric02 <elric.milet@gmail.com>
2026-04-02 17:11:09 +0200

    Fix crash on empty initial temp_df

--- koda_lib.py
+++ koda_lib.py
@@ -98,10 +98,10 @@ def import_timeframe(provider, date, time_ranges, import_method="online", realti
                     route_short_name_list.append(-1)
                     route_type_list.append(-1)
             temp_df = temp_df.with_columns([
-                pl.Series("route_id", routes_list),
-                pl.Series("direction_id", directions_list),
-                pl.Series("route_short_name", route_short_name_list, strict=False),
-                pl.Series("route_type", route_type_list),
+                pl.Series("route_id", routes_list, dtype=pl.Utf8),
+                pl.Series("direction_id", directions_list, dtype=pl.Int64),
+                pl.Series("route_short_name", route_short_name_list, dtype=pl.Utf8),
+                pl.Series("route_type", route_type_list, dtype=pl.Int64),
             ])
             
             total_df = pl.concat([total_df, temp_df], how="diagonal")
